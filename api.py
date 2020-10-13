4a1f61dd43e41ba6a98c26582872a0d5937603ae
863334858 <59336266+863334858@users.noreply.github.com>
2020-10-13 13:26:39 +0800

    Update api.py

--- api.py
+++ api.py
@@ -153,7 +153,7 @@ class WeiBo:
     def check_in(self, s, topic_dict):
         """
         微博国际版APP签到接口
-        :param s: 通过抓取微博国际版签到请求获取
+        :param s: 通过抓取微博国际版签到、请求获取
         :param topic_dict: 超话信息字典,具体格式请看get_topic_list函数
         :return:
         """
